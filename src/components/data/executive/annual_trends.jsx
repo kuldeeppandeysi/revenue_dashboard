6618db0f480e8ddcdb69977a8e10b8fb1691c808
Sidhant Bahl <sidhantbahl@SIGU-AP-23-N-34.local>
2025-10-13 12:30:15 +0530

    Made some data changes

--- src/components/data/executive/annual_trends.jsx
+++ src/components/data/executive/annual_trends.jsx
@@ -1,38 +1,41 @@
 export default [
 {
 "label": "2023",
-"live_arr": 8661303,
+"live_arr": 590789796,
 "contracted_arr": null,
+"accrued_mrr": null,
 "target_arr": null,
 "live_clients": 206,
-"nrr": 84,
-"rule_of_80": 0,
-"gm_percent": 70.4,
-"ebitda_percent": -13.9,
-"headcount": 316
+"nrr": null,
+"rule_of_80": null,
+"gm_percent": null,
+"ebitda_percent": null,
+"headcount": null
 },
 {
 "label": "2024",
-"live_arr": 8661303,
+"live_arr": 731880070,
 "contracted_arr": null,
+"accrued_mrr": 645731342,
 "target_arr": null,
 "live_clients": 206,
 "nrr": 84,
-"rule_of_80": 0,
+"rule_of_80": null,
 "gm_percent": 70.4,
 "ebitda_percent": -13.9,
 "headcount": 316
 },
 {
 "label": "2025",
-"live_arr": 8661303,
+"live_arr": 772686257,
 "contracted_arr": null,
+"accrued_mrr": 225358841,
 "target_arr": null,
 "live_clients": 206,
 "nrr": 84,
-"rule_of_80": 0,
-"gm_percent": 70.4,
-"ebitda_percent": -13.9,
+"rule_of_80": null,
+"gm_percent": 61.7,
+"ebitda_percent": -26.0,
 "headcount": 316
 }
 ];
--- src/components/data/executive/finance_data.jsx
+++ src/components/data/executive/finance_data.jsx
@@ -1,104 +1,104 @@
 export default [
 {
-    "Unbilled": 186035134.867322,
-    "AR": 145038195.26999998,
-    "Total Outstanding": 331073330.13732195,
+    "Unbilled": 186035135,
+    "AR": 145038195,
+    "Total Outstanding": 331073330,
     "Month": "Apr-2024"
   },
   {
-    "Unbilled": 199890360.852522,
-    "AR": 139101365.14000008,
-    "Total Outstanding": 338991725.99252206,
+    "Unbilled": 199890361,
+    "AR": 139101365,
+    "Total Outstanding": 338991726,
     "Month": "May-2024"
   },
   {
-    "Unbilled": 204435346.027722,
-    "AR": 136973470.93,
-    "Total Outstanding": 341408816.957722,
+    "Unbilled": 204435346,
+    "AR": 136973471,
+    "Total Outstanding": 341408817,
     "Month": "Jun-2024"
   },
   {
-    "Unbilled": 215052547.939374,
-    "AR": 106066444.61999996,
-    "Total Outstanding": 321118992.559374,
+    "Unbilled": 215052548,
+    "AR": 106066445,
+    "Total Outstanding": 321118993,
     "Month": "Jul-2024"
   },
   {
-    "Unbilled": 229084632.506724,
-    "AR": 401973341.22000015,
-    "Total Outstanding": 631057973.7267241,
+    "Unbilled": 229084633,
+    "AR": 401973341,
+    "Total Outstanding": 631057974,
     "Month": "Aug-2024"
   },
 {
-    "Unbilled": 227753409.849037,
-    "AR": 120164936.64000003,
-    "Total Outstanding": 347918346.48903704,
+    "Unbilled": 227753410,
+    "AR": 120164937,
+    "Total Outstanding": 347918346,
     "Month": "Sep-2024"
   },
   {
-    "Unbilled": 221830534.024237,
-    "AR": 152226631.80999982,
-    "Total Outstanding": 374057165.83423686,
+    "Unbilled": 221830534,
+    "AR": 152226632,
+    "Total Outstanding": 374057166,
     "Month": "Oct-2024"
   },
   {
-    "Unbilled": 211637764.839437,
-    "AR": 168102223.51999992,
-    "Total Outstanding": 379739988.3594369,
+    "Unbilled": 211637765,
+    "AR": 168102224,
+    "Total Outstanding": 379739988,
     "Month": "Nov-2024"
   },
   {
-    "Unbilled": 220294602.554637,
-    "AR": 184011774.14999995,
-    "Total Outstanding": 404306376.70463693,
+    "Unbilled": 220294603,
+    "AR": 184011774,
+    "Total Outstanding": 404306377,
     "Month": "Dec-2024"
   },
   {
-    "Unbilled": 248015371.790159,
-    "AR": 154609826.64999995,
-    "Total Outstanding": 402625198.44015896,
+    "Unbilled": 248015372,
+    "AR": 154609827,
+    "Total Outstanding": 402625198,
     "Month": "Jan-2025"
   },
   {
-    "Unbilled": 249466308.714779,
-    "AR": 172909819.2799999,
-    "Total Outstanding": 422376127.9947789,
+    "Unbilled": 249466309,
+    "AR": 172909819,
+    "Total Outstanding": 422376128,
     "Month": "Feb-2025"
   },
   {
-    "Unbilled": 228432505.695237,
-    "AR": 191992825.6999998,
-    "Total Outstanding": 420425331.39523685,
+    "Unbilled": 228432506,
+    "AR": 191992826,
+    "Total Outstanding": 420425331,
     "Month": "Mar-2025"
   },
   {
-    "Unbilled": 218625211.069929,
-    "AR": 205388455.0499999,
-    "Total Outstanding": 424013666.1199289,
+    "Unbilled": 218625211,
+    "AR": 205388455,
+    "Total Outstanding": 424013666,
     "Month": "Apr-2025"
   },
   {
-    "Unbilled": 262447805.27347666,
-    "AR": 133763345.72000006,
-    "Total Outstanding": 396211150.99347675,
+    "Unbilled": 262447805,
+    "AR": 133763346,
+    "Total Outstanding": 396211151, 
     "Month": "May-2025"
   },
   {
-    "Unbilled": 241156115.79681,
-    "AR": 212047702.2099999,
-    "Total Outstanding": 453203818.0068099,
+    "Unbilled": 241156116,
+    "AR": 212047702,
+    "Total Outstanding": 453203818,
     "Month": "Jun-2025"
   },
   {
-    "Unbilled": 160864138.495135,
-    "AR": 152379054.63999996,
-    "Total Outstanding": 313243193.13513494,
+    "Unbilled": 160864138,
+    "AR": 152379055,
+    "Total Outstanding": 313243193,
     "Month": "Jul-2025"
   },
   {
-    "Unbilled": 169926632.246418,
-    "AR": 176293575.78999987,
-    "Total Outstanding": 346220208.03641784,
+    "Unbilled": 169926632,
+    "AR": 176293576,
+    "Total Outstanding": 346220208,
     "Month": "Aug-2025"
   }
 ]
